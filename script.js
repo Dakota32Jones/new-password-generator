6cbdfd4dae44bb13fe479ce8dc39104184337486
Dakota Jones <dakota32jones@gmail.com>
2022-07-03 15:17:46 -0500

    restarted JS code. Was encountering too many errors. Updated code with correct function prompts when you click generate code button

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecialChar;
+var userChoices;
 
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Various Arrays user can select from
-
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "=",
-  "+",
-  "<",
-  ">",
-  "/",
-  "?",
-];
-var alphaLower = [
+var lowerCase = [
   "a",
   "b",
   "c",
@@ -63,100 +36,93 @@ var alphaLower = [
   "y",
   "z",
 ];
-var alphaUpper = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+
+// variables to upper case and make sure to have the arrays for the password for the user to choose from.
+
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+
+upperCase = lowerCase.map(toUpper);
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var special = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  " < ",
+  "=",
+  " > ",
+  " ? ",
+  "@",
+  "[",
+  "\\",
+  "]",
+  " ^ ",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
 ];
 
-// Declare variables
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
-
-// Add prompt for user to confirm how many characters they would like in their password.
-
-function generatePassword() {
-  var confirmLength = prompt(
-    "How many characters would you like your password to contain?"
-  );
-
-  // make sure to loop the prompt if the answers are outside of the criteria.
-
-  while (confirmLength <= 7 || confirmLength >= 129) {
-    alert(
-      "Password length must be between 8-128 characters. Please try again."
-    );
-    var confirmLength = prompt(
-      "How many characters would you like your password to contain?"
-    );
-  }
+  passwordText.value = password;
 }
 
-// make an alert to repeat back to the user how many characters they will have in their password.
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-alert(`Your password will have ${confirmLength} characters`);
+// start generate password function. Needs to be called upon and ask for input from user.
 
-// Setting the criteria for the password
-
-var confirmSpecialCharacter = confirm(
-  "Click OK to confirm if you would like special characters. If not click Cancel."
-);
-var confirmNumericCharacter = confirm(
-  "Click OK to confirm if you would like numeric characters. If not click Cancel."
-);
-var confirmLowerCase = confirm(
-  "Click OK to confirm if you would like lowercase characters. If not click Cancel."
-);
-var confirmUpperCase = confirm(
-  "Click OK to confirm if you would like uppercase characters. If not click Cancel."
-);
-
-// need to loop if the password does not meet the password criteria.
-
-while (
-  confirmUpperCase === false &&
-  confirmNumericCharacter === false &&
-  confirmLowerCase === false &&
-  confirmSpecialCharacter === false
-) {
-  alert("You must choose at lease one parameter");
-  var confirmSpecialCharacter = confirm(
-    "Click OK to confirm if you would like special characters. If not click Cancel."
+function generatePassword() {
+  passwordLength = prompt(
+    "Password must contain 8-128 characters. How many characters would you like your password to be?"
   );
-  var confirmNumericCharacter = confirm(
-    "Click OK to confirm if you would like numeric characters. If not click Cancel."
-  );
-  var confirmLowerCase = confirm(
-    "Click OK to confirm if you would like lowercase characters. If not click Cancel."
-  );
-  var confirmUpperCase = confirm(
-    "Click OK to confirm if you would like uppercase characters. If not click Cancel."
-  );
+
+  if (!passwordLength) {
+    alert("Required Value");
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt(
+      "Sorry you must choose a password that has 8-128 characters.Please try again."
+    );
+
+    console.log("Password length" + passwordLength);
+  } else {
+    confirmLower = confirm(
+      "Would you like your password to contain lower case letters. If YES, please hit OK. If no, please hit cancel."
+    );
+
+    confirmNumber = confirm(
+      "Would you like your password to contain numbers? If YES, please hit OK. If no, please hit cancel."
+    );
+
+    confirmSpecialChar = confirm(
+      "Would you like your password to contain special characters? If YES, please hit OK. If no, please hit cancel."
+    );
+
+    confirmUpper = confirm(
+      "Would you like your password to contain uppercase letters? If YES, please hit OK. If not please hit cancel."
+    );
+  }
 }
